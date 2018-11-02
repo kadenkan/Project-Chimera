2be5603e5581e5458d2cf3214b29539364b6f77a
Kaden <KKen9413@gmail.com>
2018-11-02 16:52:05 +0800

    finish initial user model

--- chimera_core/models.py
+++ chimera_core/models.py
@@ -8,32 +8,35 @@ from django.contrib.auth.hashers import PBKDF2PasswordHasher
 class UserManager(BaseUserManager):
     use_in_migrations = True
 
-    def _create_user(self, userName, email, rawPw, **extra_fields):
-        userName = self.normalize_username(userName)
-        email = self.normalize_email(email)
-        user = self.model(userName=userName, **extra_fields)
-        hashedPw = user.make_password(rawPw, None, PBKDF2PasswordHasher)
-        user.set_password(hashedPw)
+    def create_user(self, userName, email, password=None):
+        user = self.model(
+            userName=userName, 
+            email=self.normalize_email(email),
+            )
+        user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, userName, email, rawPw, **extra_fields):
-        extra_fields.setdefault('is_superuser', True)
-
-        if extra_fields.get('is_superuser') is not True:
-            raise ValueError('Superuser must have is_superuser=True.')
-
-        return self._create_user(userName, email, rawPw, **extra_fields)
+    def create_superuser(self, userName, email, password=None):
+        user = self.create_user(
+            userName,
+            email,
+            password=password,
+        )
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
 
 
 class User(AbstractBaseUser):
     userId = models.AutoField(primary_key=True)
-    userName = models.CharField('user name', max_length=20, unique=True)
-    email = models.EmailField('email address', max_length=20)
-    password = models.CharField('hashed password', max_length=500)
-    salt = models.CharField('salt', max_length=100)
+    userName = models.CharField(verbose_name='user name', max_length=20, unique=True)
+    email = models.EmailField(verbose_name='email address', max_length=20)
 
     objects = UserManager()
 
     USERNAME_FIELD = 'userName'
     REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
